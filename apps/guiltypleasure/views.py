9bb9b662e2d198711a1f480c20bcda30cf9caba3
Sean <beattys_1@yahoo.com>
2016-08-23 09:40:17 -0700

    views
    laid out the views

--- apps/guiltypleasure/views.py
+++ apps/guiltypleasure/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import User, Product, Address, Billing, Category, Image, Order, Review, Comment
 
 def index(request):
     return render (request, 'guiltypleasure/index.html')
