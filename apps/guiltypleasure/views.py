2e6fcd7debae8499703d6b388cf5da4fd88a55be
Sean <beattys_1@yahoo.com>
2016-08-23 09:51:36 -0700

    oh sorry
    this is the real map for views

--- apps/guiltypleasure/views.py
+++ apps/guiltypleasure/views.py
@@ -3,3 +3,24 @@ from .models import User, Product, Address, Billing, Category, Image, Order, Rev
 
 def index(request):
     return render (request, 'guiltypleasure/index.html')
+
+def show_product(request, id):
+    return render(request, 'guiltypleasure/show.html')
+
+def cart(request):
+    pass
+
+def admin_index(request):
+    pass
+
+def admin_log(request):
+    pass
+
+def orders(request):
+    pass
+
+def show_order(request):
+    pass
+
+def products(request):
+    pass
